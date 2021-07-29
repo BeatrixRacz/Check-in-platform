c37c9749f181c990a4178bc3258c68fe66b13420
Beatrix <beatrix.racz00@e-uvt.ro>
2021-07-29 19:40:17 +0300

    just empty components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ScheduleComponent } from './components/schedule/schedule.component';
+import {CalendarComponent} from './components/calendar/calendar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    HeaderComponent,
+    ScheduleComponent
   ],
   imports: [
     BrowserModule,
